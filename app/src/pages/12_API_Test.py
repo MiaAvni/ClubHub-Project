edf0c249375cf60460c82d27a07b347101538af0
corrinnaweyhrich <116926271+corrinnaweyhrich@users.noreply.github.com>
2025-12-04 16:39:42 -0500

    created new pages files

--- app/src/pages/12_API_Test.py
+++ app/src/pages/12_API_Test.py
@@ -8,18 +8,23 @@ from modules.nav import SideBarLinks
 SideBarLinks()
 
 st.write("# Accessing a REST API from Within Streamlit")
-"""
-Simply retrieving data from a REST api running in a separate Docker Container.
 
-If the container isn't running, this will be very unhappy.  But the Streamlit app 
-should not totally die. 
-"""
 
-data = {} 
+searches = requests.get('http://api:4000/clubs/searches').json()
+
 try:
-  data = requests.get('http://web-api:4000/data').json()
+  st.dataframe(searches)
 except:
-  st.write("**Important**: Could not connect to sample api, so using dummy data.")
-  data = {"a":{"b": "123", "c": "hello"}, "z": {"b": "456", "c": "goodbye"}}
+  st.write('Could not connect to database to retrieve searches')
 
-st.dataframe(data)
+
+
+
+# data = {} 
+# try:
+#   data = requests.get('http://web-api:4000/data').json()
+# except:
+#   st.write("**Important**: Could not connect to sample api, so using dummy data.")
+#   data = {"a":{"b": "123", "c": "hello"}, "z": {"b": "456", "c": "goodbye"}}
+
+# st.dataframe(data)
