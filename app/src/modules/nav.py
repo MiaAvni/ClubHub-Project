1a0b155905b4b29d23b99017b8ed627c4978c843
corrinnaweyhrich <116926271+corrinnaweyhrich@users.noreply.github.com>
2025-12-06 12:53:02 -0500

    added icons for buttons

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 # ----------------------------eboard--------------------------
 def EBoardNav():
-    st.sidebar.page_link("pages/20_EBoard_Home.py", label="E-Board Home", icon='👥')
+    st.sidebar.page_link("pages/20_EBoard_Home.py", label="E-Board Home", icon='🏠')
 
 def interestedstudents():
     st.sidebar.page_link("pages/21_Interested_Students.py", label="Interested Students", icon='🎓')
@@ -36,114 +36,114 @@ def AboutPageNav():
 #### ------------------------ data analyst willow  ------------------------
 def DataAnalystHomeNav():
     st.sidebar.page_link(
-        "pages/40_willow_home.py", label="Data Analyst Home"
+        "pages/40_willow_home.py", label="Data Analyst Home", icon='🏠'
     )
 
 
 def SearchData():
     st.sidebar.page_link(
-        "pages/41_willow_searches.py", label="Search Data"
+        "pages/41_willow_searches.py", label="Search Data", icon='🔎'
     )
 
 
 def AppsData():
     st.sidebar.page_link(
-        "pages/42_willow_applications.py", label="Application Data"
+        "pages/42_willow_applications.py", label="Application Data", icon='📝'
     )
 
 def CategoriesData():
     st.sidebar.page_link(
-        "pages/43_willow_categories.py", label="Categories Data"
+        "pages/43_willow_categories.py", label="Categories Data", icon='🏷️'
     )
 
 def DemographicsData():
     st.sidebar.page_link(
-        "pages/44_willow_demographics.py", label="Demographics Data"
+        "pages/44_willow_demographics.py", label="Demographics Data", icon='📈'
     )
 
 def AttendeesData():
     st.sidebar.page_link(
-        "pages/45_willow_attendees.py", label="Attendees Data"
+        "pages/45_willow_attendees.py", label="Attendees Data", icon='👥'
     )
 
 #### ------------------------ student alex  ------------------------
 def StudentHomeNav():
     st.sidebar.page_link(
-        "pages/50_alex_Home.py", label="Student Home (Alex)", icon="📚"
+        "pages/50_alex_Home.py", label="Student Home", icon="🏠"
     )
 
 
 def AlexClubsNav():
     st.sidebar.page_link(
-        "pages/51_alex_clubs.py", label="Find Clubs"
+        "pages/51_alex_clubs.py", label="Find Clubs", icon="🔎"
     )
 
 
 def AlexApplicationsNav():
     st.sidebar.page_link(
-        "pages/52_alex_applications.py", label="My Applications"
+        "pages/52_alex_applications.py", label="My Applications", icon="📝"
     )
 
 
 def AlexNewApplicationNav():
     st.sidebar.page_link(
-        "pages/53_alex_new_application.py", label="Submit Application"
+        "pages/53_alex_new_application.py", label="Submit Application", icon="✅"
     )
 
 
 def AlexUpdateApplicationNav():
     st.sidebar.page_link(
-        "pages/54_alex_update_application.py", label="Update Application"
+        "pages/54_alex_update_application.py", label="Update Application", icon="🔄"
     )
 
 
 def AlexDeleteApplicationNav():
     st.sidebar.page_link(
-        "pages/55_alex_delete_application.py", label="Delete Application"
+        "pages/55_alex_delete_application.py", label="Delete Application", icon="🗑️"
     )
 
 #### ------------------------ admin elizabeth  ------------------------
 def adminNav():
     st.sidebar.page_link(
-        "pages/60_administrator_home.py", label="Administrator Home (Elizabeth)"
+        "pages/60_administrator_home.py", label="Administrator Home", icon="🏠"
     )
 
 def adminpermissions():
     st.sidebar.page_link(
-        "pages/61_admin_permissions_directory.py", label="Administrator Permissions Directory"
+        "pages/61_admin_permissions_directory.py", label="Administrator Permissions Directory", icon="🚦"
     )
 def adminupdatedirectory():
     st.sidebar.page_link(
-        "pages/62_update_directory.py", label="Update Directory"
+        "pages/62_update_directory.py", label="Update Directory", icon="🛠️"
     )
 
 def adminerrordirectory():
     st.sidebar.page_link(
-        "pages/62_update_directory.py", label="Update Directory"
+        "pages/63_error_directory.py", label="Error Directory", icon="⚠️"
     )
 
 def syserrordirectory():
     st.sidebar.page_link(
-        "pages/64_network_system_error_directory.py", label="System Error Directory"
+        "pages/64_network_system_error_directory.py", label="System Error Directory", icon="⛔"
     )
 
 def changeupdatestatus():
     st.sidebar.page_link(
-        "pages/65_change_update_status.py", label="Change Update Status"
+        "pages/65_change_update_status.py", label="Change Update Status", icon="🛠️"
     )
 
 def createpermissions():
     st.sidebar.page_link(
-        "pages/66_create_admin_permissions.py", label="Create Admin Permissions"
+        "pages/66_create_admin_permissions.py", label="Create Admin Permissions", icon="✅"
     )
 
 def createnotification():
     st.sidebar.page_link(
-        "pages/67_create_notifications.py", label="Create Notification"
+        "pages/67_create_notifications.py", label="Create Notification", icon="🔔"
     )
 def admincontact():
     st.sidebar.page_link(
-        "pages/68_eboard_admin_contact.py", label="Contact Eboard"
+        "pages/68_eboard_admin_contact.py", label="Contact Eboard", icon="📚"
     )
 
 # --------------------------------Links Function -----------------------------------------------
